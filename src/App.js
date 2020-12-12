f1415217a87c3fdff3c4c4c801bb66c651d2cf40
Abhishek Amin <amin.abhi297@gmail.com>
2020-12-12 06:38:17 +0530

    format code using prettier

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import logo from "./logo.svg";
+import "./App.css";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
 function App() {
   return (
